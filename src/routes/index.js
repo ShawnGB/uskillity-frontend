8677bcbcca224921fe24ae737defe0fe98e23278
Bojan Nikolov <bnikolov@melontech.com>
2017-12-18 16:15:38 +0100

    Routes

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from '../components/home/Home';
+import About from '../components/about/About';
+import Login from '../components/login/Login';
+import Register from '../components/register/Register';
+
+export default () => (
+  <Router>
+    <div>
+      <Route exact path='/' component={Home} />
+      <Route path='/about' component={About} />
+      <Route path='/login' component={Login} />
+      <Route path='/register' component={Register} />
+    </div>
+  </Router>
+);
