db43e8e62895a02d39a72251ca0d209e267fd982
Sajjad Pervaiz <sajjad@kotaico.de>
2017-12-20 15:20:39 +0100

    Added, footer, dashboard, user profile components

---
+++ src/components/dashboard/profile/ProfileForm.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+export default class ProfileForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render() {
+    <div>
+      <h1>Profil Erstellen</h1>
+    </div>
+  }
+}
---
+++ src/components/footer/FooterForm.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class FooterForm extends Component {
+  render(){
+    return (
+      <div>
+        <h1>Hello! I am a Footer</h1>
+      </div>
+    );
+  }
+}
