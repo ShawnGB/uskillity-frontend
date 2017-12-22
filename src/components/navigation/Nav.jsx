b410fab4485dd9642784f0f4a461121a59c29904
Bojan Nikolov <bnikolov@melontech.com>
2017-12-22 16:16:49 +0100

    Custom styles

--- src/components/navigation/Nav.jsx
+++ src/components/navigation/Nav.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './style.css';
 
 class Navbar extends React.Component {
   render() {
     return (
       <div className='container'>
-        <nav class='navbar fixed-top navbar-default'>
-          <div class='container-fluid'>
-            <div class='navbar-header'>
+        <nav className='navbar fixed-top navbar-default'>
+          <div className='container-fluid'>
+            <div className='navbar-header'>
               <img src='http://placehold.it/300x60?text=Logo' width='250' height='60' alt='' />
             </div>
-            <ul class='nav navbar-nav navbar-center'>
-              <li><Link to='/'>Home</Link></li>
-              <li><Link to='/register'>Register</Link></li>
+            <ul className='nav navbar-nav navbar-center'>
+              <li className='menu-item'><Link to='/register'>Register now</Link></li>
+              <li className='menu-item'><Link to='/login'>Log in</Link></li>
             </ul>
-            <ul class='nav navbar-nav navbar-right'>
-              <li><Link to='/login'>Login</Link></li>
-              <li><Link to='/about'>About</Link></li>
+            <ul className='nav navbar-nav navbar-right'>
+              <li className='menu-item'><Link to='/shareyourskill'>Share your skill</Link></li>
+              <li className='menu-item'><Link to='/'>Learn a Skill</Link></li>
             </ul>
           </div>
         </nav>
--- src/components/navigation/NavLogged.jsx
+++ src/components/navigation/NavLogged.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './style.css';
 
 class NavLogged extends React.Component {
   render() {
     return (
       <div className='container'>
-        <nav class='navbar fixed-top navbar-default'>
-          <div class='container-fluid'>
-            <div class='navbar-header'>
+        <nav className='navbar fixed-top navbar-default'>
+          <div className='container-fluid'>
+            <div className='navbar-header'>
               <img src='http://placehold.it/300x60?text=Logo' width='250' height='60' alt='' />
             </div>
-            <ul class='nav navbar-nav navbar-center'>
-              <li><Link to='/shareyourskill'>Share your skill</Link></li>
-              <li><Link to='/'>Learn a Skill</Link></li>
+            <ul className='nav navbar-nav navbar-center'>
+              <li className='menu-item'><Link to='/shareyourskill'>Share your skill</Link></li>
+              <li className='menu-item'><Link to='/'>Learn a Skill</Link></li>
             </ul>
-            <ul class='nav navbar-nav navbar-right'>
-              <li><Link to='/about'>u/about</Link></li>
-              <li><Link to='/'>My Account</Link></li>
-              <li><Link to='/'>Log out</Link></li>
+            <ul className='nav navbar-nav navbar-right'>
+              <li className='menu-item'><Link to='/about'>u/about</Link></li>
+              <li className='menu-item'><Link to='/'>My Account</Link></li>
+              <li className='menu-item'><Link to='/'>Log out</Link></li>
             </ul>
           </div>
         </nav>
