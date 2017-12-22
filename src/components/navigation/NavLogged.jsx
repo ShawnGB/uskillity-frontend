a685c13e31cfaef253572835eaa6f04306330821
Bojan Nikolov <bnikolov@melontech.com>
2017-12-22 14:29:36 +0100

    bootstrap styling for logged navbar

--- src/components/navigation/NavLogged.jsx
+++ src/components/navigation/NavLogged.jsx
@@ -4,14 +4,23 @@ import { Link } from 'react-router-dom';
 class NavLogged extends React.Component {
   render() {
     return (
-      <div>
-        <ul>
-          <li><Link to='/shareyourskill'>Share your skill</Link></li>
-          <li><Link to='/'>Learn a Skill</Link></li>
-          <li><Link to='/about'>u/about</Link></li>
-          <li><Link to='/'>My Account</Link></li>
-          <li><Link to='/'>Log out</Link></li>
-        </ul>
+      <div className='container'>
+        <nav class='navbar fixed-top navbar-default'>
+          <div class='container-fluid'>
+            <div class='navbar-header'>
+              <img src='http://placehold.it/300x60?text=Logo' width='250' height='60' alt='' />
+            </div>
+            <ul class='nav navbar-nav navbar-center'>
+              <li><Link to='/shareyourskill'>Share your skill</Link></li>
+              <li><Link to='/'>Learn a Skill</Link></li>
+            </ul>
+            <ul class='nav navbar-nav navbar-right'>
+              <li><Link to='/about'>u/about</Link></li>
+              <li><Link to='/'>My Account</Link></li>
+              <li><Link to='/'>Log out</Link></li>
+            </ul>
+          </div>
+        </nav>
       </div>
     );
   }
