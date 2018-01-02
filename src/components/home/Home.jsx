1b2744b70cd5142db0dec2ab725d70dfc9e23a8c
Bojan Nikolov <bnikolov@melontech.com>
2018-01-02 16:17:37 +0100

    Elements on home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -10,6 +10,35 @@ class Home extends React.Component {
           <div className='jumbotron'>
             <img src='http://placehold.it/900x300?text=Img' width='100%' height='100%' alt='' />
           </div>
+          <div className='row row-home'>
+            <div className='col-sm-4'>
+              <img src='http://placehold.it/350x220?text=Img' width='350' height='220' alt='' />
+            </div>
+            <div className='col-sm-4'>
+              <img src='http://placehold.it/350x220?text=Img' width='350' height='220' alt='' />
+            </div>
+            <div className='col-sm-4'>
+              <img src='http://placehold.it/350x220?text=Img' width='350' height='220' alt='' />
+            </div>
+          </div>
+          <div className='row row-home'>
+            <div className='col-sm-4'>
+              <img src='http://placehold.it/350x220?text=Img' width='350' height='220' alt='' />
+            </div>
+            <div className='col-sm-4'>
+              <img src='http://placehold.it/350x220?text=Img' width='350' height='220' alt='' />
+            </div>
+            <div className='col-sm-4'>
+              <img src='http://placehold.it/350x220?text=Img' width='350' height='220' alt='' />
+            </div>
+          </div>
+          <div className='row'>
+            <div className='about-home'>
+              <p className='about-home-header'>ABOUT US</p>
+              <p className='about-home-content'>... Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit </p>
+              <p className='about-home-content'>... Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit </p>
+            </div>
+          </div>
         </div>
       </div>
     );
