cd4b9d9a30c07ea1c299a9c8b9ea13543b8344f9
Bojan Nikolov <bnikolov@melontech.com>
2018-01-02 15:39:29 +0100

    Profile courses styles and classes

--- src/components/dashboard/profile/ProfileContent.jsx
+++ src/components/dashboard/profile/ProfileContent.jsx
@@ -6,7 +6,7 @@ class ProfileContent extends React.Component {
   render() {
     return (
       <div>
-        <div className='container'>
+        <div className='container container-profile'>
           <div className="row">
             <div className="col-lg-3">
             <div className='img-container'>
--- src/components/dashboard/profile/ProfileCourses.jsx
+++ src/components/dashboard/profile/ProfileCourses.jsx
@@ -4,7 +4,38 @@ class ProfileCourses extends React.Component {
   render() {
     return (
       <div>
-        Profile Courses
+        <div className='container container-profile'>
+          <p className='skills-heading'>My Shared Courses</p>
+          <div className='row'>
+            <div className='col-sm-3'>
+              <img src='http://placehold.it/300x60?text=Img' width='250' height='180' alt='' />
+              <div className='skill-content'>
+                <p className='skill-category'>Arts & Crafts</p>
+                <p className='skill-title'>Aquarell painting for beginners</p>
+                <p className='skill-author'>Marina Berlin-Kreuzberg</p>
+                <p className='skill-price'>14 €</p>
+              </div>
+            </div>
+            <div className='col-sm-3'>
+              <img src='http://placehold.it/300x60?text=Img' width='250' height='180' alt='' />
+              <div className='skill-content'>
+                <p className='skill-category'>Arts & Crafts</p>
+                <p className='skill-title'>Aquarell painting for beginners</p>
+                <p className='skill-author'>Marina Berlin-Kreuzberg</p>
+                <p className='skill-price'>14 €</p>
+              </div>
+            </div>
+            <div className='col-sm-3'>
+              <img src='http://placehold.it/300x60?text=Img' width='250' height='180' alt='' />
+              <div className='skill-content'>
+                <p className='skill-category'>Arts & Crafts</p>
+                <p className='skill-title'>Aquarell painting for beginners</p>
+                <p className='skill-author'>Marina Berlin-Kreuzberg</p>
+                <p className='skill-price'>14 €</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
