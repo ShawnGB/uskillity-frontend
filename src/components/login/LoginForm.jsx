96bdc9e2b7646dfcaaeac53464a0f2da92de2711
Andreas Leicher <email@andreasleicher.com>
2018-01-09 12:26:45 +0100

    feat(login): (WIP) FB JS SDK and login button added

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,3 +1,4 @@
+/*global FB*/
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { service } from '../../services/service'
@@ -14,6 +15,87 @@ class LoginForm extends Component {
     };
     this.onChange = this.onChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    const APP_ID = "618914508265405"
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : APP_ID,
+        cookie     : true,  // enable cookies to allow the server to access the session
+        xfbml      : true,  // parse social plugins on this page
+        version    : 'v2.11' // use version 2.11
+      });
+
+      // Now that we've initialized the JavaScript SDK, we call
+      // FB.getLoginStatus().  This function gets the state of the
+      // person visiting this page and can return one of three states to
+      // the callback you provide.  They can be:
+      //
+      // 1. Logged into your app ('connected')
+      // 2. Logged into Facebook, but not your app ('not_authorized')
+      // 3. Not logged into Facebook and can't tell if they are logged into
+      //    your app or not.
+      //
+      // These three cases are handled in the callback function.
+      FB.getLoginStatus(function(response) {
+        this.statusChangeCallback(response);
+      }.bind(this));
+    }.bind(this);
+
+    // Load the SDK asynchronously
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = `https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=${APP_ID}`;
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }
+
+  testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+    });
+  }
+
+  // This is called with the results from from FB.getLoginStatus().
+  statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      this.testAPI();
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      document.getElementById('status').innerHTML = 'Please log ' +
+      'into this app.';
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      document.getElementById('status').innerHTML = 'Please log ' +
+      'into Facebook.';
+    }
+  }
+
+  // This function is called when someone finishes with the Login
+  // Button.  See the onlogin handler attached to it in the sample
+  // code below.
+  checkLoginState() {
+    console.log("checking login state");
+    FB.getLoginStatus(function(response) {
+      this.statusChangeCallback(response);
+    }.bind(this));
+  }
+
+  handleFBbuttonClick() {
+    console.log("handling button click")
+    FB.login(this.checkLoginState());
   }
 
   onChange(e) {
@@ -39,7 +121,10 @@ class LoginForm extends Component {
               <form>
                 <fieldset>
                   <div className="form-group">
-                    <button className='btn btn-lg btn-facebook btn-block' type='button'>
+                    <div id='social-login-button-facebook'>
+                      <div className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="true" onClick={() => this.handleFBbuttonClick()}></div>
+                    </div>
+                    <button className='btn btn-lg btn-facebook btn-block' type='button' onClick={() => this.handleFBbuttonClick()}>
                       Log in with Facebook
                     </button>
                     <label style={{ margin: '5% 0 5% 50%'}}> or </label>
