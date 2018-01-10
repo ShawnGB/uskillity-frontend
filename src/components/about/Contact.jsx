0bbfceaff3c3a6f4c1415d32c5ab057a41e97169
Bojan Nikolov <bnikolov@melontech.com>
2018-01-10 14:16:49 +0100

    Contact, help and Press pages

--- src/components/about/Contact.jsx
+++ src/components/about/Contact.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import Sidebar from './Sidebar';
+import Navbar from '../navigation/Nav';
+import Footer from '../footer/Footer';
 import './style.css';
 
 export default class Contact extends React.Component{
   render(){
     return(
       <div>
-        <div className='container'>
+        <Navbar />
+        <div className='container about-container'>
           <div className='row'>
-            <div className="col-lg-9">
-              <h2 className='about-heading'> Contact </h2>
+            <div className='col-md-2'>
+              <Sidebar />
+            </div>
+            <div className='col-md-8'>
+              <h2 className='about-heading'>Contact</h2>
               <p className='about-content'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
               </p>
@@ -20,6 +27,7 @@ export default class Contact extends React.Component{
               </p>
             </div>
           </div>
+          <Footer />
         </div>
       </div>
     );
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,8 @@ import About from "../components/about/About";
 import Vision from "../components/about/Vision";
 import Team from "../components/about/Team";
 import Contact from "../components/about/Contact";
+import Help from "../components/about/Help";
+import Press from "../components/about/Press";
 import Courses from "../components/courses/Courses";
 import Profile from "../components/dashboard/profile/Profile";
 import ProfileEdit from "../components/dashboard/profile/ProfileEdit";
@@ -21,6 +23,8 @@ export default () => (
       <Route path="/team" component={Team} />
       <Route path="/courses" component={Courses} />
       <Route path="/contact" component={Contact} />
+      <Route path="/help" component={Help} />
+      <Route path="/press" component={Press} />
       <Route exact path="/profile/edit" component={ProfileEdit} />
       <Route exact path="/profile" component={Profile} />
       <Route path="/workshop" component={Workshop} />
