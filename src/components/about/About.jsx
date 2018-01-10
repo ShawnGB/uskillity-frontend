0e999451fc7c77a33368195ebfe16a0b5a20f799
Bojan Nikolov <bnikolov@melontech.com>
2018-01-10 10:21:16 +0100

    Linking components from Sidebar

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Guidelines from './Guidelines'
+import Guidelines from './Guidelines';
+import Sidebar from './Sidebar';
 import Navbar from '../navigation/Nav';
 import Footer from '../footer/Footer';
 import { Link } from 'react-router-dom';
@@ -12,27 +13,7 @@ class About extends React.Component {
         <div className="container about-container">
           <div className='row'>
             <div className='col-md-2'>
-              <ul
-                className="nav nav-stacked">
-                <li className="active">
-                  <Link to='/vision'>Vision</Link>
-                </li>
-                <li>
-                  <Link to='/team'>Team</Link>
-                </li>
-                <li>
-                  <Link to='/contact'>Contact</Link>
-                </li>
-                <li>
-                  <Link to='/press'>Press</Link>
-                </li>
-                <li>
-                  <Link to='/help'>Help</Link>
-                </li>
-                <li>
-                  <Link to='/guidelines'>Guidelines</Link>
-                </li>
-              </ul>
+              <Sidebar />
             </div>
             <div className='col-md-8'>
               <Guidelines />
