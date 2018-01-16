c0e043bb3fea2a1a936611ed2f8b488b30fe4e13
Sajjad Pervaiz <sajjad@kotaico.de>
2018-01-16 17:04:45 +0100

    Catetories and workshops are dynamically loaded now

--- src/components/dashboard/LearnSkill.jsx
+++ src/components/dashboard/LearnSkill.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Navbar from '../navigation/Nav';
 import Footer from '../../components/footer/Footer';
 import './style.css';
@@ -9,46 +9,63 @@ class LearnSkill extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      workshops: []
+      categories: {}
     };
   }
-  componentDidMount(){
-    fetch(`${SERVER}/workshops.json`)
-    .then((resp) => {
+  componentDidMount() {
+    fetch(`${SERVER}/workshops.json`).then((resp) => {
       if (!resp.ok) {
         // TODO: send back to home page
       }
       return resp.json();
-    })
-    .then((workshops) => {
-      console.log("workshops",workshops);
-      this.setState({workshops})
+    }).then((workshops) => {
+      fetch(`${SERVER}/categories.json`).then((resp) => {
+        resp.json().then((categories_data) => {
+          let categories = {}
+          for (var i = 0; i < workshops.length; i++) {
+            let category_id = workshops[i].category_id
+            if (!categories[category_id]) {
+              categories[category_id] = {}
+              categories[category_id].workshops = []
+              let category = categories_data.find(c => c.id === category_id)
+              categories[category_id].name = category.name
+            }
+            categories[category_id].workshops.push(workshops[i]);
+          }
+          this.setState({categories})
+          console.log("categories", categories);
+        });
+      })
+      console.log("workshops", workshops);
     });
   }
 
   render() {
-    const { workshops } = this.state;
-    return(
-      <div>
-        <Navbar />
-        <div className='container'>
-        {workshops.map((data,i) =>(
-          <div className='col-sm-4' key={i}>
-            <p className='skills-heading'>Category</p>
-            <img src='http://placehold.it/350x220?text=Img' width='350' height='220' alt='' />
-            <div className='skill-content'>
-              <p className='skill-title'>{data.title}</p>
-              <p className='skill-author'>Marina Berlin-Kreuzberg</p>
-              <p className='skill-price'>14 €</p>
-            </div>
-          </div>
-        ))}
-
-          <Footer />
-        </div>\
+    const {categories} = this.state;
+    return <div>
+      <Navbar/>
+      <div className='container'>
+        {Object.keys(categories).map((key, i) => (<CategoryRow name={categories[key].name} workshops={categories[key].workshops} key={i}/>))}
+        <Footer/>
       </div>
-    )
+    </div>
   }
+}
+
+const CategoryRow = props => {
+  return <div className='row'>
+    <p className='skills-heading'>{props.name}</p>
+    {
+      props.workshops.map((workshop, i) => (<div className="col-sm-4" key={i}>
+        <img src={workshop.main_image} width='350' height='220' alt=''/>
+        <div className='skill-content'>
+          <p className='skill-title'>{workshop.title}</p>
+          <p className='skill-author'>Marina Berlin-Kreuzberg</p>
+          <p className='skill-price'>14 €</p>
+        </div>
+      </div>))
+    }
+  </div>
 }
 
 export default LearnSkill;
