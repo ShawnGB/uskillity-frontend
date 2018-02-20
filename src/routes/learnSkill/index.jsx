57197bdc3e97e8d2eb70937fa72df7210473166d
Sandeep Sadanandan <sillyfellow@whybenormal.org>
2018-02-20 16:27:00 +0100

    fix(build):

--- src/routes/learnSkill/index.jsx
+++ src/routes/learnSkill/index.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
 import * as skillActions from "app:store/actions/skill";
 import Slider from "react-slick";
 import "../shareSkill/style.css";
