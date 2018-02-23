4b7331d3bbe0b78157e567793c1af1bb6f8979d9
Sandeep Sadanandan <sillyfellow@whybenormal.org>
2018-02-23 18:17:18 +0100

    update login modal css

--- src/components/register-modal/index.jsx
+++ src/components/register-modal/index.jsx
@@ -1,23 +1,17 @@
 import React from "react";
-import { translate, Trans } from "react-i18next";
+import { translate } from "react-i18next";
 import { Modal } from "react-bootstrap";
 import RegisterForm from "./RegisterForm";
 
 const RegisterModal = props => {
   return (
     <Modal show={true} onHide={props.hideModal}>
-      <Modal.Header closeButton>
-        <Modal.Title>
-          <Trans i18nKey="register.modal.title">uSkillity</Trans>
-        </Modal.Title>
-      </Modal.Header>
       <Modal.Body>
         <RegisterForm
           handleSubmit={props.handleSubmit}
           jumpToModal={props.jumpToModal}
         />
       </Modal.Body>
-      <Modal.Footer />
     </Modal>
   );
 };
