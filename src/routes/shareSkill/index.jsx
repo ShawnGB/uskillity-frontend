d74e7f53e5088a0b11c6dd82188f9d116accb2c1
Stefanos Voulgarakis <stefanos.voulgarakis@gmail.com>
2018-03-01 12:00:15 +0100

    fix(shareskill): check if null instead of undefined

--- src/routes/shareSkill/index.jsx
+++ src/routes/shareSkill/index.jsx
@@ -89,21 +89,22 @@ class ShareSkill extends Component {
     // TODO: If nothing really changed why PUT??
     const { dispatch } = this.props;
     if (session.id && this.props.editable) {
-        dispatch(
-          skillActions.updateWorkshopSession(this.state.workshopId, session)
-        );
+      dispatch(
+        skillActions.updateWorkshopSession(this.state.workshopId, session)
+      );
     } else if (this.addingSessionCompleted(session)) {
       const { workshops } = this.props.skills;
-      let workshopId = this.state.workshopId || workshops[workshops.length - 1].id;
+      let workshopId =
+        this.state.workshopId || workshops[workshops.length - 1].id;
       dispatch(skillActions.saveWorkshopSession(workshopId, session));
     }
   };
 
   addingSessionCompleted(session) {
     let ret = true;
-    ret &= session["starts_at"] !== undefined;
-    ret &= session["ends_at"] !== undefined;
-    ret &= session["dateAndTime"] !== undefined;
+    ret &= session["starts_at"] !== null;
+    ret &= session["ends_at"] !== null;
+    ret &= session["dateAndTime"] !== null;
     return ret === 1;
   }
 
@@ -157,7 +158,7 @@ class ShareSkill extends Component {
     e.preventDefault();
     if (this.props.editable) {
       skillActions.updateWorkshop(this.state.workshop, this.state.workshopId);
-    }else {
+    } else {
       this.props.dispatch(skillActions.saveWorkshop(this.state.workshop));
     }
   }
@@ -166,7 +167,7 @@ class ShareSkill extends Component {
     const { skills, session, t } = this.props;
     const levels = skills.levels;
     const categories = skills.categories;
-    const add_session = this.props.editable? true : skills.add_session;
+    const add_session = this.props.editable ? true : skills.add_session;
     const isLoggedIn = session && session.isLoggedIn;
     const initialWorkshop = this.state.initialWorkshop;
     return (
@@ -420,71 +421,76 @@ class ShareSkill extends Component {
               </div>
 
               <div>
-                {add_session?(<div>
-                  <div className="row share-skill-row">
-                    <div className="col-xs-12 skills-form-label">
-                      <span className="skills-form-title">
-                        <Trans i18nKey="share_skill.date_and_time_label">
-                          Date and time
-                        </Trans>
-                      </span>
-                    </div>
-                    {this.state.sessions.map((session, index) => (
-                      <ScheduleWorkshop
-                        onChange={this.onChangeWorkshopSession.bind(this, index)}
-                        onBlur={this.updateWorkshopSession.bind(
-                          this,
-                          session,
-                          index
-                        )}
-                        key={index}
-                        session={session}
-                      />
-                    ))}
-                    <div className="col-xs-3">
-                      <div className="row share-skill-row">
-                        <div className="col-xs-12">
-                          <button
-                            type="button"
-                            className="btn btn-default btn-sm skills-select-box add-session-button"
-                            onClick={this.addRow}
-                            style={{ borderRadius: "17px" }}
-                          >
-                            <span
-                              className="glyphicon glyphicon-plus"
-                              style={{ fontSize: "15px" }}
-                            />
-                          </button>
+                {add_session ? (
+                  <div>
+                    <div className="row share-skill-row">
+                      <div className="col-xs-12 skills-form-label">
+                        <span className="skills-form-title">
+                          <Trans i18nKey="share_skill.date_and_time_label">
+                            Date and time
+                          </Trans>
+                        </span>
+                      </div>
+                      {this.state.sessions.map((session, index) => (
+                        <ScheduleWorkshop
+                          onChange={this.onChangeWorkshopSession.bind(
+                            this,
+                            index
+                          )}
+                          onBlur={this.updateWorkshopSession.bind(
+                            this,
+                            session,
+                            index
+                          )}
+                          key={index}
+                          session={session}
+                        />
+                      ))}
+                      <div className="col-xs-3">
+                        <div className="row share-skill-row">
+                          <div className="col-xs-12">
+                            <button
+                              type="button"
+                              className="btn btn-default btn-sm skills-select-box add-session-button"
+                              onClick={this.addRow}
+                              style={{ borderRadius: "17px" }}
+                            >
+                              <span
+                                className="glyphicon glyphicon-plus"
+                                style={{ fontSize: "15px" }}
+                              />
+                            </button>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="row share-skill-row">
-                    <div className="col-xs-12 skills-form-label">
-                      <span className="skills-form-title">Photo</span>
+                    <div className="row share-skill-row">
+                      <div className="col-xs-12 skills-form-label">
+                        <span className="skills-form-title">Photo</span>
+                      </div>
+                      <div className="col-xs-12">
+                        <form name="form">
+                          <div className="form-group">
+                            <SkillInputSingle
+                              type="file"
+                              onChange={this.handleImageChange.bind(this)}
+                            />
+                            <button
+                              onClick={this.saveWorkshopCover.bind(this)}
+                              type="button"
+                              className="btn btn-default btn-sm skills-select-box"
+                              style={{ width: "140px", float: "right" }}
+                            >
+                              <Trans i18nKey="share_skill.button_upload_picture">
+                                Upload a cover photo
+                              </Trans>
+                            </button>
+                          </div>
+                        </form>
+                      </div>
                     </div>
-                    <div className="col-xs-12">
-                      <form name="form">
-                        <div className="form-group">
-                          <SkillInputSingle
-                            type="file"
-                            onChange={this.handleImageChange.bind(this)}
-                          />
-                          <button
-                            onClick={this.saveWorkshopCover.bind(this)}
-                            type="button"
-                            className="btn btn-default btn-sm skills-select-box"
-                            style={{ width: "140px", float: "right" }}
-                          >
-                            <Trans i18nKey="share_skill.button_upload_picture">
-                              Upload a cover photo
-                            </Trans>
-                          </button>
-                        </div>
-                      </form>
-                    </div>
                   </div>
-                </div>):null}
+                ) : null}
                 <div className="checkbox">
                   <label>
                     <input
