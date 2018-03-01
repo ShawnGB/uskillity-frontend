1d3252a4e4a4c77a5011f24a956e3e71a10440b4
Stefanos Voulgarakis <stefanos.voulgarakis@gmail.com>
2018-03-01 17:40:10 +0100

    Revert "fix(i18n): select en when not de"
    This reverts commit 76bab8895f38235786bf485b18c21021ed54c508.

--- src/routes/home/index.jsx
+++ src/routes/home/index.jsx
@@ -75,45 +75,46 @@ class Home extends React.Component {
     console.log("navigator.language", navigator.language);
     if (navigator.language.includes("de")) {
       this.props.i18n.changeLanguage("de");
-    } else {
-      this.props.i18n.changeLanguage("en");
     }
+    //else {
+    //this.props.i18n.changeLanguage("en");
+    //}
   }
 
   render() {
     return (
       <div>
-        <CustomCarousel items={this.state.workshops} />
-        <div className="row row-home category-collection">
-          {this.state.categories}
-        </div>
-        <div className="row">
-          <div className="about-home">
-            <p className="about-home-header">
-              <Trans i18nKey="home.header">ABOUT US</Trans>
-            </p>
-            <p className="about-home-content">
-              <Trans i18nKey="home.body_0">
-                ... Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                diam nonumy eirmod tempor invidunt ut labore et dolore magna
-                aliquyam erat, sed diam voluptua. At vero eos et accusam et
-                justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-                takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
-                dolor sit
-              </Trans>
-            </p>
-            <p className="about-home-content">
-              <Trans i18nKey="home.body_1">
-                ... Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                diam nonumy eirmod tempor invidunt ut labore et dolore magna
-                aliquyam erat, sed diam voluptua. At vero eos et accusam et
-                justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-                takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
-                dolor sit
-              </Trans>
-            </p>
+          <CustomCarousel items={this.state.workshops} />
+          <div className="row row-home category-collection">
+            {this.state.categories}
           </div>
-        </div>
+          <div className="row">
+            <div className="about-home">
+              <p className="about-home-header">
+                <Trans i18nKey="home.header">ABOUT US</Trans>
+              </p>
+              <p className="about-home-content">
+                <Trans i18nKey="home.body_0">
+                  ... Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                  sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                  magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+                  et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+                  no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
+                  ipsum dolor sit
+                </Trans>
+              </p>
+              <p className="about-home-content">
+                <Trans i18nKey="home.body_1">
+                  ... Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                  sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                  magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+                  et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+                  no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
+                  ipsum dolor sit
+                </Trans>
+              </p>
+            </div>
+          </div>
       </div>
     );
   }
