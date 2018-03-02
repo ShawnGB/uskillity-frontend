c47b0db1ce0c240c58b5f3d58e3fb693b52705ee
Sandeep Sadanandan <sillyfellow@whybenormal.org>
2018-03-02 23:40:29 +0100

    update workshop view

--- src/routes/workshop/index.jsx
+++ src/routes/workshop/index.jsx
@@ -31,71 +31,68 @@ class Workshop extends React.Component {
     let level = levels.find(i => i.id === workshop.level_id) || {};
     return (
       <div>
-        <div className="container">
-          <div className="jumbotron">
-            <img src={workshop.images[0]} width="100%" height="60%" alt="" />
-          </div>
-          <div className="row row-spacing">
-            <div className="col-sm-9">
-              <p className="workshop-name">{workshop.title}</p>
-              <p>{workshop.full_address}</p>
-                <div className="row row-spacing">
-                  <div className="col-sm-2">
-                    <p className="workshop-title">
-                      <Trans i18nKey="workshop.recommended_label">Recommended</Trans>
-                    </p>
-                  </div>
-                </div>
-                <div className="row row-spacing">
-                  <div className="col-sm-3">
-                    <ul>
-                      <li>
-                        Age: {workshop.min_age}
-                        {" - "}
-                        {workshop.max_age}
-                      </li>
-                      <li>Other: </li>
-                      <li>Level: {level.name}</li>
-                    </ul>
-                  </div>
-                  <div className="col-sm-3">
-                    <div>
-                      Sessions:{workshop.sessions.map((session, i) => (
-                        <p key={i}>
-                          {" "}
-                          {util.parseSessionDateTime(
-                            session.starts_at,
-                            "DD.MM.YY"
-                          )}{" "}
-                          {util.parseSessionDateTime(session.starts_at)}
-                          {" - "}
-                          {util.parseSessionDateTime(session.ends_at)}
-                        </p>
-                      ))}
-                    </div>
-                  </div>
-                </div>
-            </div>
-            <div className="col-sm-3">
-              <Sidebar workshop={this.state.workshop} />
-            </div>
-            <div className="row row-spacing">
-              <div className="col-md-9">
+        <div className="workshop-main-image" style={{ backgroundImage: `url(${workshop.images[0]})` }} />
+        <div className="row">
+          <div className="col-sm-9">
+            <p className="workshop-name">{workshop.title}</p>
+            <p>{workshop.full_address}</p>
+            <div className="row">
+              <div className="col-sm-2">
                 <p className="workshop-title">
-                  <Trans i18nKey="workshop.description_label">Description</Trans>
-                </p>
-                <p className="workshop-content">{workshop.description}</p>
-                <p className="workshop-title">
-                  <Trans i18nKey="workshop.about_the_instructor_label">
-                    About the instructor
+                  <Trans i18nKey="workshop.recommended_label">
+                    Recommended
                   </Trans>
                 </p>
-                <p className="workshop-content">{workshop.provider.about}</p>
-                <p className="workshop-content">{workshop.about}</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-sm-3">
+                <ul>
+                  <li>
+                    Age: {workshop.min_age}
+                    {" - "}
+                    {workshop.max_age}
+                  </li>
+                  <li>Other: </li>
+                  <li>Level: {level.name}</li>
+                </ul>
+              </div>
+              <div className="col-sm-3">
+                <div>
+                  Sessions:{workshop.sessions.map((session, i) => (
+                    <p key={i}>
+                      {" "}
+                      {util.parseSessionDateTime(
+                        session.starts_at,
+                        "DD.MM.YY"
+                      )}{" "}
+                      {util.parseSessionDateTime(session.starts_at)}
+                      {" - "}
+                      {util.parseSessionDateTime(session.ends_at)}
+                    </p>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
-
+          <div className="col-sm-3">
+            <Sidebar workshop={this.state.workshop} />
+          </div>
+          <div className="row row-spacing">
+            <div className="col-md-9">
+              <p className="workshop-title">
+                <Trans i18nKey="workshop.description_label">Description</Trans>
+              </p>
+              <p >{workshop.description}</p>
+              <p className="workshop-title">
+                <Trans i18nKey="workshop.about_the_instructor_label">
+                  About the instructor
+                </Trans>
+              </p>
+              <p >{workshop.provider.about}</p>
+              <p >{workshop.about}</p>
+            </div>
+          </div>
         </div>
       </div>
     );
