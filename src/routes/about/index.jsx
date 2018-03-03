065189a0909eab7b6bdf2225bbd00b9f58a81a06
Sandeep Sadanandan <sillyfellow@whybenormal.org>
2018-03-03 14:24:44 +0100

    fix(about) update css/navigation to be per design + minimally responsive

--- src/routes/about/index.jsx
+++ src/routes/about/index.jsx
@@ -16,69 +16,89 @@ import TermsConditions from "app:components/about/TermsConditions";
 class About extends React.Component {
   render() {
     return (
-      <div>
-        <div className="container about-container">
-          <Tab.Container id="left-tabs" defaultActiveKey={this.props.subpath}>
-            <Row className="clearfix">
-              <Col sm={3}>
-                <Nav stacked>
-                  <NavItem className="full-width-nav-item" eventKey="vision">
-                    <Trans i18nKey="about.nav.vision">Vision</Trans>
-                  </NavItem>
-                  <NavItem className="full-width-nav-item" eventKey="team">
-                    <Trans i18nKey="about.nav.team">Team</Trans>
-                  </NavItem>
-                  <NavItem className="full-width-nav-item" eventKey="contact">
-                    <Trans i18nKey="about.nav.contact">Contact</Trans>
-                  </NavItem>
-                  <NavItem className="full-width-nav-item" eventKey="press">
-                    <Trans i18nKey="about.nav.press">Press</Trans>
-                  </NavItem>
-                  <NavItem className="full-width-nav-item" eventKey="help">
-                    <Trans i18nKey="about.nav.help">Help</Trans>
-                  </NavItem>
-                  <NavItem
-                    className="full-width-nav-item"
-                    eventKey="guidelines"
-                  >
-                    <Trans i18nKey="about.nav.guidelines">Guidelines</Trans>
-                  </NavItem>
-                  <NavItem className="full-width-nav-item" eventKey="terms">
-                    <Trans i18nKey="about.nav.termsandconditions">
-                      Terms and Conditions
-                    </Trans>
-                  </NavItem>
-                </Nav>
-              </Col>
-              <Col sm={9}>
-                <Tab.Content animation>
-                  <Tab.Pane eventKey="vision">
-                    <Vision />
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="team">
-                    <Team />
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="contact">
-                    <Contact />
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="press">
-                    <Press />
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="help">
-                    <Help />
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="guidelines">
-                    <Guidelines />
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="terms">
-                    <TermsConditions />
-                  </Tab.Pane>
-                </Tab.Content>
-              </Col>
-            </Row>
-          </Tab.Container>
-        </div>
-      </div>
+      <Tab.Container id="left-tabs" defaultActiveKey={this.props.subpath}>
+        <Row className="clearfix">
+          <Col xs={12} smHidden mdHidden lgHidden>
+            <Nav justified>
+              <NavItem style={{float: "left"}} eventKey="vision">
+                <Trans i18nKey="about.nav.vision">Vision</Trans>
+              </NavItem>
+              <NavItem style={{float: "left"}} eventKey="team">
+                <Trans i18nKey="about.nav.team">Team</Trans>
+              </NavItem>
+              <NavItem style={{float: "left"}} eventKey="contact">
+                <Trans i18nKey="about.nav.contact">Contact</Trans>
+              </NavItem>
+              <NavItem style={{float: "left"}} eventKey="press">
+                <Trans i18nKey="about.nav.press">Press</Trans>
+              </NavItem>
+              <NavItem style={{float: "left"}} eventKey="help">
+                <Trans i18nKey="about.nav.help">Help</Trans>
+              </NavItem>
+              <NavItem style={{float: "left"}} eventKey="guidelines">
+                <Trans i18nKey="about.nav.guidelines">Guidelines</Trans>
+              </NavItem>
+              <NavItem style={{float: "left"}} eventKey="terms">
+                <Trans i18nKey="about.nav.termsandconditions">
+                  Terms and Conditions
+                </Trans>
+              </NavItem>
+            </Nav>
+          </Col>
+          <Col sm={3} xsHidden>
+            <Nav stacked>
+              <NavItem eventKey="vision">
+                <Trans i18nKey="about.nav.vision">Vision</Trans>
+              </NavItem>
+              <NavItem eventKey="team">
+                <Trans i18nKey="about.nav.team">Team</Trans>
+              </NavItem>
+              <NavItem eventKey="contact">
+                <Trans i18nKey="about.nav.contact">Contact</Trans>
+              </NavItem>
+              <NavItem eventKey="press">
+                <Trans i18nKey="about.nav.press">Press</Trans>
+              </NavItem>
+              <NavItem eventKey="help">
+                <Trans i18nKey="about.nav.help">Help</Trans>
+              </NavItem>
+              <NavItem eventKey="guidelines">
+                <Trans i18nKey="about.nav.guidelines">Guidelines</Trans>
+              </NavItem>
+              <NavItem eventKey="terms">
+                <Trans i18nKey="about.nav.termsandconditions">
+                  Terms and Conditions
+                </Trans>
+              </NavItem>
+            </Nav>
+          </Col>
+          <Col sm={9} xs={12}>
+            <Tab.Content animation>
+              <Tab.Pane eventKey="vision">
+                <Vision />
+              </Tab.Pane>
+              <Tab.Pane eventKey="team">
+                <Team />
+              </Tab.Pane>
+              <Tab.Pane eventKey="contact">
+                <Contact />
+              </Tab.Pane>
+              <Tab.Pane eventKey="press">
+                <Press />
+              </Tab.Pane>
+              <Tab.Pane eventKey="help">
+                <Help />
+              </Tab.Pane>
+              <Tab.Pane eventKey="guidelines">
+                <Guidelines />
+              </Tab.Pane>
+              <Tab.Pane eventKey="terms">
+                <TermsConditions />
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
     );
   }
 }
