bc5973a65693357fecb1bd0aae3adc12d04983d6
Sandeep Sadanandan <sillyfellow@whybenormal.org>
2018-03-06 22:53:40 +0100

    point back the deployment to staging

--- src/routes/version/index.jsx
+++ src/routes/version/index.jsx
@@ -5,10 +5,10 @@ class Version extends Component {
   render() {
     return (
       <div>
-        <h4>Revision: 7ad826e4d242662e359444b177b9cdb558c7324d</h4>
-        <h4>Version count: 428</h4>
-        <h4>Built on: 2018-03-06 (22:53:35), @ birdOfPrey.local</h4>
-        <h4>Running against: </h4>
+        <h4>Revision: 7423048bd3d9b3db5b4e505badff9052584b9e56</h4>
+        <h4>Version count: 388</h4>
+        <h4>Built on: 2018-03-05 (12:19:45), @ orion-syndicate</h4>
+        <h4>Running against: https://bluecarbuncle-staging.herokuapp.com</h4>
       </div>
     );
   }
