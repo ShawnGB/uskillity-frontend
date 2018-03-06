e9084de4fa728e2eb7db36cfea4950ed9949a4f7
Stefanos Voulgarakis <stefanos.voulgarakis@gmail.com>
2018-03-06 19:48:32 +0100

    deploy to production

--- src/routes/version/index.jsx
+++ src/routes/version/index.jsx
@@ -5,10 +5,10 @@ class Version extends Component {
   render() {
     return (
       <div>
-        <h4>Revision: 7423048bd3d9b3db5b4e505badff9052584b9e56</h4>
-        <h4>Version count: 388</h4>
-        <h4>Built on: 2018-03-05 (12:19:45), @ orion-syndicate</h4>
-        <h4>Running against: https://bluecarbuncle-staging.herokuapp.com</h4>
+        <h4>Revision: 4cd09a5a0fe86b20c3612b116555051add08d806</h4>
+        <h4>Version count: 421</h4>
+        <h4>Built on: 2018-03-06 (19:48:32), @ stefanos-Note</h4>
+        <h4>Running against: </h4>
       </div>
     );
   }
