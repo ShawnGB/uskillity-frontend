d6c6bc7a7a8d6aeeeb76f1be025f1322cd10b0c6
Sandeep Sadanandan <sillyfellow@whybenormal.org>
2018-03-09 15:48:02 +0100

    feat(reservation): allow users to reserve tickets

--- src/routes/workshop/Sidebar.jsx
+++ src/routes/workshop/Sidebar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Instructor from "./Instructor";
+import Participations from "./participations";
 
 class Sidebar extends React.Component {
   render() {
     return (
       <div>
         <Instructor workshop={this.props.workshop} />
+        <Participations workshop={this.props.workshop} dispatch={this.props.dispatch}/>
       </div>
     );
   }
