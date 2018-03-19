c7cdd903a071fbdcece95e70552edfec7bac56de
Sajjad Pervaiz <sajjad@kotaico.de>
2018-03-19 17:08:17 +0100

    first name can't be empty

--- src/routes/profile/Profile.jsx
+++ src/routes/profile/Profile.jsx
@@ -134,6 +134,9 @@ class Profile extends React.Component {
                       className="btn btn-primary btn-margin"
                       type="button"
                       onClick={() => this.saveEdit()}
+                      disabled={
+                        !provider.first_name
+                      }
                     >
                       Save
                     </button>
@@ -226,7 +229,11 @@ const ProfileEditable = props => {
           defaultValue={props.provider.first_name}
           placeholder={props.t("profile.first_name_placeholder")}
           onChange={props.handleEdit}
-          hintless
+          demand={"Too short"}
+          hint={props.t("profile.first_name_hint_text")}
+          validate={c => {
+            return validateContentByLength(c, 4, 32);
+          }}
         />
       </div>
       <div className="col-xs-6">
