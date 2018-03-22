7229e7aacb9fc563474d2ac01fc35fe674879314
Sajjad Pervaiz <sajjad@kotaico.de>
2018-03-22 20:17:43 +0100

    added workshop rating

---
+++ src/routes/workshop/WorkshopRating.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Rating from "react-rating";
+import * as skillActions from "app:store/actions/skill";
+
+class WorkshopRating extends React.Component {
+  onChange(value) {
+    const { dispatch } = this.props;
+    dispatch(skillActions.submitRating(value, this.props.workshop.id));
+  }
+  render() {
+    return (
+      <Rating
+        emptySymbol="glyphicon glyphicon-star-empty"
+        fullSymbol="glyphicon glyphicon-star"
+        initialRating={this.props.workshop.rating}
+        onChange={this.onChange.bind(this)}
+      />
+    );
+  }
+}
+
+export default WorkshopRating;
