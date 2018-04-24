0002778027c4b0612f124070b4cc41ca5845bb40
Sandeep Sadanandan <sillyfellow@whybenormal.org>
2018-04-24 14:30:27 +0200

    update version

--- src/routes/version/index.jsx
+++ src/routes/version/index.jsx
@@ -5,9 +5,9 @@ class Version extends Component {
   render() {
     return (
       <div>
-        <h4>Revision: 90d087e0173398ecdf95ed7ae56799c79bad9204</h4>
-        <h4>Version count: 479</h4>
-        <h4>Built on: 2018-03-28 (19:38:49), @ birdOfPrey.local</h4>
+        <h4>Revision: ccfdc6e2848c09a1aa272fb75dcdc19f41c788ca</h4>
+        <h4>Version count: 602</h4>
+        <h4>Built on: 2018-04-24 (14:29:30), @ birdOfPrey.local</h4>
         <h4>Running against: </h4>
       </div>
     );
