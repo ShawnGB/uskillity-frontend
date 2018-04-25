83408a4d181c1aa769488683d0cd6c670316c628
Sandeep Sadanandan <sillyfellow@whybenormal.org>
2018-04-25 12:02:34 +0200

    point back the deployment to staging

--- src/routes/version/index.jsx
+++ src/routes/version/index.jsx
@@ -5,9 +5,9 @@ class Version extends Component {
   render() {
     return (
       <div>
-        <h4>Revision: 2b611bfd51a17f8e0e0f5a4fbc46cce54f6268e0</h4>
-        <h4>Version count: 607</h4>
-        <h4>Built on: 2018-04-25 (12:00:54), @ birdOfPrey.local</h4>
+        <h4>Revision: ccfdc6e2848c09a1aa272fb75dcdc19f41c788ca</h4>
+        <h4>Version count: 602</h4>
+        <h4>Built on: 2018-04-24 (14:29:30), @ birdOfPrey.local</h4>
         <h4>Running against: </h4>
       </div>
     );
@@ -15,4 +15,3 @@ class Version extends Component {
 }
 
 export default Version;
-
