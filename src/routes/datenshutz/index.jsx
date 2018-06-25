a9368d5e0b8706725c250a2249f6ea8e8e851d7d
Shawm GB <shawn@uskillity.de>
2018-06-25 13:55:27 +0200

    fixed Privacy Policy loading

--- src/routes/datenshutz/index.jsx
+++ src/routes/datenshutz/index.jsx
@@ -1,6 +1,31 @@
 import React, { Component } from "react";
 
 class Datenschutz extends Component {
+
+  constructor(props) {
+    super();
+  }
+
+  componentWillMount() {
+    (function (w,d) {
+      let loader = function () {
+        let s = d.createElement("script"),tag = d.getElementsByTagName("script")[0];
+        s.src="https://cdn.iubenda.com/iubenda.js";
+        tag.parentNode.insertBefore(s,tag);
+      };
+      if(w.addEventListener){
+        w.addEventListener("load", loader, false);
+      }
+      else if(w.attachEvent){
+        w.attachEvent("onload", loader);
+      }
+      else{w.onload = loader;
+      }
+    }
+  )(window, document)
+  }
+
+
   render() {
     return (
       <div>
@@ -8,22 +33,6 @@ class Datenschutz extends Component {
           class="iubenda-white iubenda-embed iub-legal-only iub-no-markup iub-body-embed"
           title="Privacy Policy">Privacy Policy
         </a>
-
-        {
-          (function (w,d) {
-            var loader = function () {
-              var s = d.createElement("script"),tag = d.getElementsByTagName("script")[0];
-              s.src="https://cdn.iubenda.com/iubenda.js";
-              tag.parentNode.insertBefore(s,tag);
-            }; if(w.addEventListener){
-              w.addEventListener("load", loader, false);
-            }else if(w.attachEvent){
-              w.attachEvent("onload", loader);
-            }else{w.onload = loader;
-            }
-          }
-        )(window, document)
-    }
       </div>
     );
   }
