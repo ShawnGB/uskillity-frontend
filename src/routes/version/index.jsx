a030852e63fcc129bbfac32a203195941a2aed3c
Shawm GB <shawn@uskillity.de>
2018-07-04 11:36:04 +0200

    deploy to production

--- src/routes/version/index.jsx
+++ src/routes/version/index.jsx
@@ -5,9 +5,9 @@ class Version extends Component {
   render() {
     return (
       <div>
-        <h4>Revision: 1c076efda34f15fa5b4afb0c833669bf2948e203</h4>
-        <h4>Version count: 655</h4>
-        <h4>Built on: 2018-06-12 (10:38:09), @ Shawns-MacBook-2.local</h4>
+        <h4>Revision: e56e9d84e0853566932b60aacc2a22a5b94cc569</h4>
+        <h4>Version count: 675</h4>
+        <h4>Built on: 2018-07-04 (11:36:04), @ Shawns-MacBook-2.local</h4>
         <h4>Running against: </h4>
       </div>
     );
