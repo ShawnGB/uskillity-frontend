61452e7a8a0b14905c6ad6e97346818395c642da
Shawm GB <shawn@uskillity.de>
2018-07-04 13:09:06 +0200

    merge master

--- src/routes/version/index.jsx
+++ src/routes/version/index.jsx
@@ -5,9 +5,9 @@ class Version extends Component {
   render() {
     return (
       <div>
-        <h4>Revision: 162cea2946fc8a2edba036ce57556b487efadab5</h4>
-        <h4>Version count: 678</h4>
-        <h4>Built on: 2018-07-04 (13:08:42), @ Shawns-MacBook-2.local</h4>
+        <h4>Revision: 1c076efda34f15fa5b4afb0c833669bf2948e203</h4>
+        <h4>Version count: 655</h4>
+        <h4>Built on: 2018-06-12 (10:38:09), @ Shawns-MacBook-2.local</h4>
         <h4>Running against: </h4>
       </div>
     );
